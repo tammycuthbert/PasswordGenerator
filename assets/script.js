74ae40a6fbc9fbcee5b1309526a13e9d65acddc2
Tammy Cuthbert <tammycuthert427@gmail.com>
2020-05-22 15:57:22 -0500

    update character set homework week3

--- assets/script.js
+++ assets/script.js
@@ -12,22 +12,31 @@ function generatePassword() {
     return generatePassword();
   }
 
+  var isUppercase = confirm("Do you want uppercase characters?");
+  var isLowercase = confirm("Do you want lowercase characters?");
+  var isSpecialCharacters = confirm("Do you want special characters?");
+  var isNumeric = confirm("Do you want numbers?");
+
+  if (isUppercase){ 
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  }
+
+  if (isLowercase){
+    charset += "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (isSpecialCharacters){
+    charset += "!@#$%^&*()_+?><:;-=";
+  }
+
+  if (isNumeric){
+    charset += "0123456789";
+  }
+
+  
+
 }
-
-var isUppercase = confirm("Do you want uppercase characters?");
-var isLowercase = confirm("Do you want lowercase characters?");
-var isSpecialCharacters = confirm("Do you want special characters?");
-var isnumeric = confirm("Do you want numbers?");
-
-if (isUppercase){ 
-  charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-}
-
-if (isLowercase){
-  charset += "abcdefghijklmnopqrstuvwxyz";
-}
-
 
 // Write password to the #password input
 function writePassword() {
