e09b45f9910d3c71527a8edd31e7ca43ae06532e
Tammy Cuthbert <tammycuthert427@gmail.com>
2020-05-22 16:08:55 -0500

    Password for loop

--- assets/script.js
+++ assets/script.js
@@ -34,7 +34,12 @@ function generatePassword() {
     charset += "0123456789";
   }
 
-  
+  var password = "";
+  for(var i = 0;  i < length; i++){
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+
+  return password;
 
 }
 
